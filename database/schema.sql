bd03acf2cd780d6aecc6251d4c8d109737f3fc3d
Anthony Cella <anthony.cella242@gmail.com>
2023-01-20 21:37:20 -0500

    Started implementing the express server. Changed the foreign key in the reports table to the newly created username column in the users table

--- database/schema.sql
+++ database/schema.sql
@@ -4,6 +4,7 @@ USE db;
 
 CREATE TABLE users (
   id INT NOT NULL AUTO_INCREMENT,
+  username varchar(255),
   first_name varchar(255),
   last_name varchar(255),
   phone_number varchar(255) NOT NULL,
@@ -11,10 +12,10 @@ CREATE TABLE users (
 
 CREATE TABLE reports (
   id INT NOT NULL AUTO_INCREMENT,
-  user_id_reporting INT NOT NULL,
+  user_reporting INT NOT NULL,
   date_created varchar(255) NOT NULL,
   status_of_report varchar(255) NOT NULL,
   last_updated varchar(255) NOT NULL,
   PRIMARY KEY (id)
-  FOREIGN KEY (user_id_reporting) REFERENCES users(id)
+  FOREIGN KEY (user_reporting) REFERENCES users(username)
 );
---
+++ server/index.js
@@ -0,0 +1,1 @@
+const express = use("express");
