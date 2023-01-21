aee4de2c1d27ca2a6485aae1ace90f15f4072695
Anthony Cella <anthony.cella242@gmail.com>
2023-01-20 21:44:04 -0500

    Created express server in index.js. Installed path

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
-const express = use("express");
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+// for connecting to the front-end
+// app.use(express.static(path.join(__dirname, '../CLIENTSIDE_DIRECTORY_PATH_GOES_HERE')));
+
+
+
+app.listen(PORT);
+console.log(`Server listening at http://localhost:${PORT}`);
