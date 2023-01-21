d780c08c740c28f469465380f0cf352380aeeac0
Anthony Cella <anthony.cella242@gmail.com>
2023-01-21 12:58:53 -0500

    Created insertion query for joint table between users and groups. Still need to test the code

--- database/queries/usersGroupsJointQueries.js
+++ database/queries/usersGroupsJointQueries.js
@@ -1,1 +1,25 @@
 const dbConnection = require('../db');
+
+const insertIntoJointTable = (req, res) => {
+  const data = req.body;
+  const { phoneNumber, groupId } = data;
+
+  const insertionQuery = `INSERT INTO users_groups VALUES ("${phoneNumber}", ${groupId})`;
+
+  dbConnection.connect((error) => {
+    if (error) {
+      console.log('Connection to database unsuccessful, from insertIntoJointTable');
+      res.status(404).send();
+    } else {
+      dbConnection.query(insertionQuery, (errorInQuery, result) => {
+        if (errorInQuery) {
+          console.log('error in query: insertIntoJointTable');
+          console.log(errorInQuery);
+          res.status(404).send();
+        } else {
+          res.status(201).send(result);
+        }
+      });
+    }
+  });
+};
