cd5026e9cf6af221bcacae4fcf89200b2d613240
Anthony Cella <anthony.cella242@gmail.com>
2023-01-20 23:56:55 -0500

    Added a query to the query.js file. Not sure if it is good or not yet but we will see

--- database/query.js
+++ database/query.js
@@ -26,4 +26,34 @@ const insertIntoReports = (req, res) => {
   });
 };
 
+const insertIntoUsers = (req, res) => {
+  const data = req.body;
+
+  const { phoneNumber } = data;
+
+  let columns = '(phone_number';
+
+  if (data.firstName) {
+    columns +=
+  }
+
+  const insertionQuery = `INSERT INTO users VALUES ("${userReporting}",
+   "${dateCreated}", "${statusOfReport}", "${dateCreated}")`;
+
+  dbConnection.connect((error) => {
+    if (error) {
+      console.log('Connection to database unsuccessful, from createNewReportInDatabase');
+      res.status(404).send();
+    } else {
+      dbConnection.query(insertionQuery, (errorInQuery, result) => {
+        if (errorInQuery) {
+          res.status(404).send();
+        } else {
+          res.status(201).send(result);
+        }
+      });
+    }
+  });
+};
+
 module.exports = insertIntoReports;
