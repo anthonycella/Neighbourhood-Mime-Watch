f71b7a3a7f7ff7dedbdad7ebf568e8f4723bc0e7
Anthony Cella <anthony.cella242@gmail.com>
2023-01-21 14:52:25 -0500

    Added a dummy data loader sql file

---
+++ database/dummydata.sql
@@ -0,0 +1,7 @@
+USE db;
+
+INSERT INTO users (phone_number, first_name, last_name, username) VALUES ("(919) 410-1939", "Bruce", "Wayne", "iambatman");
+INSERT INTO users (phone_number, first_name, last_name, username) VALUES ("(915) 555-0199", "Darlene", "McKinnon", "monstertruckgal87");
+INSERT INTO users (phone_number, first_name, last_name, username) VALUES ("0-188-999-881-999-119-725-3", "Maurice", "Moss", "negative1");
+INSERT INTO users (phone_number, first_name, last_name, username) VALUES ("(070) 412-9084", "Abigail", "Lytar", "uganda4life");
+INSERT INTO users (phone_number, first_name, last_name, username) VALUES ("(912) 059-9119", "Billy", "Blazes", "ready2roll");
