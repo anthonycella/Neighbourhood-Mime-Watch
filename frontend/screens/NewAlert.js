905c667fc967f5da2f0e39710e39d61623e6cc0d
kethnal <kaseyvaldez@hotmail.com>
2023-01-21 00:40:38 -0800

    Create function for alerting group members

--- frontend/screens/NewAlert.js
+++ frontend/screens/NewAlert.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native-web';
 import TextInput2 from '../components/TextInput2';
 import Button from '../components/Button';
 import Header from '../components/Header';
 
 function NewAlert(props) {
+  const [alertSent, setAlertSent] = useState(false);
+
+  function handleAlertGroup() {
+    
+  }
+
   return (
     <View>
-      <Header text="create new alert"/>
-      <TextInput2 placeholder="group"/>
-      <TextInput2 height={300} multiline={true} placeholder="what's the activity?"/>
-      <Button buttonText="alert group"/>
+      {!alertSent ?
+      <>
+        <Header text="create new alert"/>
+        <TextInput2 placeholder="group"/>
+        <TextInput2 height={300} multiline={true} placeholder="what's the activity?"/>
+        <Button buttonText="alert group"/>
+      </>
+      :
+      <>
+        <Header text="create new alert"/>
+        <Button onPress={handleAlertGroup} buttonText="alert group"/>
+      </>}
     </View>
   );
 }
 
 export default NewAlert;
-
-// const styles = StyleSheet.create({
-//   textBody: {
-//     height: 100
-//   }
-// });
