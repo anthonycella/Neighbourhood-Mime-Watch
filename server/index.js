8f448c515e7a4e6ca07eae2e94dba01c9493a490
Anthony Cella <anthony.cella242@gmail.com>
2023-01-21 02:08:25 -0500

    Added all API endpoints

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const query = require('../database/query');
+
 const app = express();
 const PORT = 3000;
 
@@ -8,7 +10,19 @@ app.use(express.json());
 // app.use(express.static(path.join(__dirname, '../CLIENTSIDE_DIRECTORY_PATH_GOES_HERE')));
 
 app.post('/reports', (req, res) => {
+  query.insertIntoReports(req, res);
+});
 
+app.post('/users', (req, res) => {
+  query.insertIntoUsers(req, res);
+});
+
+app.put('/reports', (req, res) => {
+  query.updateReportStatus(req, res);
+});
+
+app.put('/users', (req, res) => {
+  query.updateUserData(req, res);
 });
 
 app.listen(PORT);
