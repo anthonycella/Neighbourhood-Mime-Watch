c0fb276da46c70ddcc7334769a53b9e5cdc317dd
Anthony Cella <anthony.cella242@gmail.com>
2023-01-20 23:40:58 -0500

    Wrote a creation query for the reports table. I am uncertain whether or not the API will be able to utilize its callback functionality (it's been a while)

--- database/query.js
+++ database/query.js
@@ -1,1 +1,29 @@
 const dbConnection = require('./db');
+
+const createNewReportInDatabase = (req, res) => {
+  const data = req.body;
+
+  const userReporting = data.username;
+  const { dateCreated } = data;
+  const statusOfReport = data.reportStatus;
+
+  const insertionQuery = `INSERT INTO reports VALUES ("${userReporting}",
+   "${dateCreated}", "${statusOfReport}", "${dateCreated}")`;
+
+  dbConnection.connect((error) => {
+    if (error) {
+      console.log('Connection to database unsuccessful, from createNewReportInDatabase');
+      res.status(404).send();
+    } else {
+      dbConnection.query(insertionQuery, (errorInQuery, result) => {
+        if (errorInQuery) {
+          res.status(404).send();
+        } else {
+          res.status(201).send(result);
+        }
+      });
+    }
+  });
+};
+
+module.exports = createNewReportInDatabase;
