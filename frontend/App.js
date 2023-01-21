879537dc63c2ba890d0358b47a958e7a652710a5
kethnal <kaseyvaldez@hotmail.com>
2023-01-20 22:43:35 -0800

    safety save

--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import LoginSignup from './screens/LoginSignup';
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello king</Text>
-      <StatusBar style="auto" />
+      <LoginSignup />
+      <Navbar />
     </View>
   );
 }
@@ -16,5 +18,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    margin: 20,
   },
 });
+
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>hello king</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
--- frontend/screens/LoginSignup.js
+++ frontend/screens/LoginSignup.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Button from '../components/Button';
+import TextInput2 from '../components/TextInput2';
+import Header from '../components/Header';
 
 function LoginSignup(props) {
   return (
-    <div>
-      
-    </div>
+    <View>
+      <Header text="neighbourhood grime watch"/>
+      <TextInput2 placeholder="username"/>
+      <TextInput2 placeholder="password"/>
+      <Button buttonText="log in"/>
+      <Button buttonText="sign up"/>
+    </View>
   );
 }
 
