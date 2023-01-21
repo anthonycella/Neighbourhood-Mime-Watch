e3f5372a67e7647ecdec679765df1f041e3c6d40
LorenzoBalderrama <balderramalorenzo25@gmail.com>
2023-01-20 22:49:03 -0700

    api endpoint for users created

---
+++ users.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/api/users', (req, res) => {
+  // Validate the request
+  // Create the user
+  // Respond to the client
+});
+
+module.exports = router;
