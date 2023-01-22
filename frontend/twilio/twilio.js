0916c6c3bd0dc37d3b6816c8246e3c56db81e1da
Anthony Cella <anthony.cella242@gmail.com>
2023-01-21 20:33:09 -0500

    moved the account SID to the process.env file

--- frontend/twilio/twilio.js
+++ frontend/twilio/twilio.js
@@ -1,7 +1,7 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Set environment variables for your credentials
 // Read more at http://twil.io/secure
-const accountSid = "ACab8838dc57a57564af7736ed9aed94a6";
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const sendingNumber = process.env.TWILIO_PHONE_NUMBER;
 const client = require("twilio")(accountSid, authToken);
