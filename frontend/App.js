dc78cd9e7596322c4fbac4ae11e719181fc49f5e
kethnal <kaseyvaldez@hotmail.com>
2023-01-21 22:26:19 -0800

    Fix buggy groupsmain page

--- frontend/App.js
+++ frontend/App.js
@@ -1,29 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import LoginSignup from './screens/LoginSignup';
-import Article from './components/Article';
-import SmallButton from './components/SmallButton';
-import Navbar from './components/Navbar';
+import { StyleSheet } from 'react-native';
 import Logs from './screens/Logs';
 import NewAlert from './screens/NewAlert';
 import GroupsMain from './screens/GroupsMain';
-import GroupsSpecific from './screens/GroupsSpecific';
-import GroupsMemberInvite from './screens/GroupsMemberInvite';
-
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import FontAwesome from 'react-native-vector-icons/FontAwesome5';
 
 const Tab = createBottomTabNavigator();
-
-// const tabBarBgColor = {
-//   ...DefaultTheme,
-//   colors: {
-//     ...DefaultTheme.colors,
-//     background: 'orange'
-//   },
-// };
 
 export default function App() {
   return (
